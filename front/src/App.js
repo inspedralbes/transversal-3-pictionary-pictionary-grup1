052663f2534c3a3be728fba03d2b39e0f2449d6b
Marti-SV <a21marsalval@inspedralbes.cat>
2023-03-06 13:44:03 +0100

    React installed

---
+++ front/src/App.js
@@ -0,0 +1,34 @@
+import logo from './logo.svg';
+import './App.css';
+import { useState, useEffect } from 'react';
+
+
+function App() {
+  const [cliente, setCliente] = useState('Boton Normal');
+  useEffect(() => {
+    if (cliente == 'Gaspar') {
+    console.log('El boton se llama “Gaspar”.');
+  }
+}, [cliente]); //Aquí se pone qué estado queremos que detecte el cambio.
+return (
+  <div className="App">
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <p>
+        Edit <code>src/App.js</code> and save to reload.
+      </p>
+      <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Learn React
+      </a>
+      <button onClick={() => setCliente('Gaspar')}>{ cliente }</button>
+    </header>
+  </div>
+);
+}
+
+export default App;
