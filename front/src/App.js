aaf857c403bb15d12f62d2b809f8f9ebf238d645
Marti-SV <a21marsalval@inspedralbes.cat>
2023-03-07 00:24:20 +0100

    board concept

--- front/src/App.js
+++ front/src/App.js
@@ -1,36 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
-import { useState, useEffect } from 'react';
-import Board from './components/Board'
-
+import logo from "./logo.svg";
+import "./App.css";
+import { useState, useEffect } from "react";
+import Board from "./components/Board";
 
 function App(socket) {
-  const [cliente, setCliente] = useState('Boton Normal');
+  const [cliente, setCliente] = useState("Boton Normal");
   useEffect(() => {
-    if (cliente == 'Gaspar') {
-    console.log('El boton se llama “Gaspar”.');
-  }
-}, [cliente]); //Aquí se pone qué estado queremos que detecte el cambio.
-return (
-  <div className="App">
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <p>
-        Edit <code>src/App.js</code> and save to reload.
-      </p>
-      <Board socket={socket}></Board>
-      <a
-        className="App-link"
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Learn React
-      </a>
-      <button onClick={() => setCliente('Gaspar')}>{ cliente }</button>
-    </header>
-  </div>
-);
+    if (cliente == "Gaspar") {
+      console.log("El boton se llama “Gaspar”.");
+    }
+  }, [cliente]); //Aquí se pone qué estado queremos que detecte el cambio.
+  return (
+    <div>
+      <Board></Board>
+    </div>
+  );
 }
 
 export default App;
--- front/src/components/Board.js
+++ front/src/components/Board.js
@@ -1,64 +1,45 @@
-// import logo from './logo.svg';
-// import './App.css';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from "react";
+import React from "react";
+import CanvasDraw from "react-canvas-draw";
 
+//REFERENCIA: https://github.com/embiem/react-canvas-draw
 
-function Board(socket) {
-  useEffect(() => {
-    var canvas = document.getElementById("myCanvas");
-    var context = canvas.getContext("2d");
-    var arrayDatos = [];
-    let x;
-    let y;
-    let isMousedown = false;
+function Board() {
+  const firstCanvas = useRef(null); //Serveix per agafar un component com a referencia
+  const secondCanvas = useRef(null);
 
-    const stopDrawing = () => { isMousedown = false; }
+  const save = () => {
+    const data = firstCanvas.current.getSaveData(); //Dona totes les coordenades utilitzades en el CanvasDraw
+    console.log(data);
+    secondCanvas.current.loadSaveData(data);
+  };
 
-    canvas.addEventListener("mousemove", function (evt) {
-      arrayDatos.push(x, y);
-      socket.emit("saveCoord", arrayDatos);
-      arrayDatos = [];
-      console.log(arrayDatos);
-      function update() {
-        if (isMousedown) {
-          const newX = evt.offsetX;
-          const newY = evt.offsetY;
+  const clear = () => {
+    firstCanvas.current.clear();
+  };
 
-          console.log(newX);
-
-          context.beginPath();
-
-          context.moveTo(x, y);
-          context.lineTo(newX, newY)
-          context.stroke();
-          x = newX;
-          y = newY;
-        }
-      }
-      update();
-    }, false);
-
-
-    //Get Mouse Position
-    canvas.addEventListener("mousedown", function (evt) {
-      x = evt.offsetX;
-      y = evt.offsetY;
-      isMousedown = true;
-    })
-
-    canvas.addEventListener("mouseup", stopDrawing);
-    canvas.addEventListener("mouseout", stopDrawing);
-  }, []);
+  const undo = () => {
+    firstCanvas.current.undo();
+  };
 
   return (
     <div className="Board">
-      <canvas id="myCanvas" width="800" height="500" style="border:1px solid #000000;"></canvas>
-
+      <button onClick={save}>Guarda</button>
+      <button onClick={clear}>Neteja</button>
+      <button onClick={undo}>Desfes</button>
+      <CanvasDraw
+        brushRadius={5}
+        brushColor={"red"}
+        hideInterface={true}
+        loadTimeOffset={0}
+        lazyRadius={5}
+        style={{ border: "4px solid #000" }}
+        ref={firstCanvas}
+      />
+      <h1>Canvas guardat:</h1>
+      <CanvasDraw hideGrid={true} disabled={true} ref={secondCanvas} />
     </div>
   );
-};
-
-
-
+}
 
 export default Board;
