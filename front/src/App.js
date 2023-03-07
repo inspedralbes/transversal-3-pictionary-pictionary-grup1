2b50355541dea95506ebb194e924237d42419066
JaumePerez <a21jaupersen@inspedralbes.cat>
2023-03-07 09:14:06 +0100

    wordCheck

--- front/src/App.js
+++ front/src/App.js
@@ -2,8 +2,25 @@ import logo from "./logo.svg";
 import "./App.css";
 import { useState, useEffect } from "react";
 import Board from "./components/Board";
+import WordForm from './components/WordForm';
+import React from 'react';
 
 function App(socket) {
+  function handleFormSubmit(word) {
+    fetch('/check-word', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'X-CSRF-TOKEN': document.querySelector('meta[name="csrf-token"]').getAttribute('content')
+      },
+      body: JSON.stringify({
+        word: word
+      })
+    })
+      .then(response => response.json())
+      .then(data => alert(data.message))
+      .catch(error => console.error(error));
+  }
   const [cliente, setCliente] = useState("Boton Normal");
   useEffect(() => {
     if (cliente == "Gaspar") {
@@ -13,6 +30,8 @@ function App(socket) {
   return (
     <div>
       <Board></Board>
+      <WordForm onSubmit={handleFormSubmit} />
+
     </div>
   );
 }
---
+++ front/src/components/WordForm.js
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+
+function WordForm(props) {
+  const [word, setWord] = useState('');
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    props.onSubmit(word);
+  }
+
+  function handleChange(event) {
+    setWord(event.target.value);
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input type="text" value={word} onChange={handleChange} placeholder="Enter a word" />
+      <button type="submit">Check</button>
+    </form>
+  );
+}
+
+export default WordForm;
