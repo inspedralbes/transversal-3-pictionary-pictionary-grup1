79d5226a2a0d9c730fb80c56247515557c750f09
GasparGomez <a21gasgomtor@inspedralbes.cat>
2023-03-07 13:08:33 +0100

    food json route fixed

--- back/tr3-laravel/app/Http/Controllers/WordController.php
+++ back/tr3-laravel/app/Http/Controllers/WordController.php
@@ -8,7 +8,7 @@ class WordController extends Controller
 {
     public function getWord()
     {
-        $jsonFile = json_decode(file_get_contents('C:\Users\Jaume\Desktop\Pictionary\transversal-3-pictionary-pictionary-grup1\back\food.json'), true);
+        $jsonFile = json_decode(file_get_contents(__DIR__.'\..\..\..\..\food.json'), true);
         $randomIndex = rand(0, count($jsonFile['food']) - 1);
         $wordToCheck = $jsonFile['food'][$randomIndex]['name'];
 
