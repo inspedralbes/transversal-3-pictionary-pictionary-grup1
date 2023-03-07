04c08a4d3f7c5ea2e613276960d78aa87800af52
AsmaeChar <a21asmchaben@inspedralbes.cat>
2023-03-07 11:42:26 +0100

    added color picker

--- front/src/components/Board.js
+++ front/src/components/Board.js
@@ -1,12 +1,21 @@
 import { useState, useEffect, useRef } from "react";
 import React from "react";
 import CanvasDraw from "react-canvas-draw";
+import { CirclePicker } from "react-color";
+
 
 //REFERENCIA: https://github.com/embiem/react-canvas-draw
 
 function Board() {
   const firstCanvas = useRef(null); //Serveix per agafar un component com a referencia
   const secondCanvas = useRef(null);
+  //Color picker
+  const [currentColor, setCurrentColor] = useState("#fff");
+
+  const handleChangeComplete = (color) => {
+    setCurrentColor(color.hex);
+  };
+
 
   const save = () => {
     const data = firstCanvas.current.getSaveData(); //Dona totes les coordenades utilitzades en el CanvasDraw
@@ -27,9 +36,10 @@ function Board() {
       <button onClick={save}>Guarda</button>
       <button onClick={clear}>Neteja</button>
       <button onClick={undo}>Desfes</button>
+      <CirclePicker color={currentColor} onChangeComplete={handleChangeComplete}></CirclePicker>  
       <CanvasDraw
         brushRadius={5}
-        brushColor={"red"}
+        brushColor={currentColor}
         hideInterface={true}
         loadTimeOffset={0}
         lazyRadius={5}
