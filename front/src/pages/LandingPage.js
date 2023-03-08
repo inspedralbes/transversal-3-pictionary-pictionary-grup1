26a152b2ba7f72630d1cc693ddccf64b804cfe0c
Marti-SV <a21marsalval@inspedralbes.cat>
2023-03-08 13:25:05 +0100

    styles file

--- front/src/pages/LandingPage.js
+++ front/src/pages/LandingPage.js
@@ -1,17 +1,18 @@
-import { NavLink, Link } from'react-router-dom';
+import { Link } from 'react-router-dom';
+import "../styles/LandingPage.css"
 
 
 function LandingPage() {
-  
-return (
-  <div>
-    LandingPage
 
-    <Link to='/lobby'><button>Create game</button></Link>
-    <Link to='/lobby'><button>Join game</button></Link>
+  return (
+    <div>
+      <h1>Pictionary</h1>
 
-  </div>
-);
+      <Link to='/lobby'><button>Create game</button></Link>
+      <Link to='/lobby'><button>Join game</button></Link>
+
+    </div>
+  );
 }
 
 export default LandingPage;
