c6308e2b79eafe0a51a9b11412eef47b6d74fd8f
unknown <a21alecrinor@inspedralbes.cat>
2023-03-09 12:17:06 +0100

    Completed merge: timer, board, check-word, screen-flow

--- front/src/pages/LandingPage.js
+++ front/src/pages/LandingPage.js
@@ -3,16 +3,6 @@ import "../styles/LandingPage.css"
 
 
 function LandingPage() {
-<<<<<<< HEAD
-
-  return (
-    <div>
-      <h1>Pictionary</h1>
-      <Link to='/createlobby'><button>Create game</button></Link>
-      <Link to='/joinlobby'><button>Join game</button></Link>
-    </div>
-  );
-=======
   
 return (
   <div>
@@ -21,14 +11,13 @@ return (
     <h1 className='ldPage__title'>Gartic Pintur.io</h1>
     
     <div className='ldPage__buttonsContainer'>
-      <Link to='/lobby'><button className='ldPage__buttons'>Create game</button></Link>
+      <Link to='/createlobby'><button className='ldPage__buttons'>Create game</button></Link>
     </div>
     <div className='ldPage__buttonsContainer'>
-      <Link to='/lobby'><button className='ldPage__buttons'>Join game</button></Link>
+      <Link to='/joinlobby'><button className='ldPage__buttons'>Join game</button></Link>
     </div>
   </div>
 );
->>>>>>> origin/screenFlow
 }
 
 export default LandingPage;
