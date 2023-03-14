9bc42377f10357647b5d11196e8f69b978b91cc3
unknown <a21alecrinor@inspedralbes.cat>
2023-03-14 09:34:14 +0100

    Added migrations and words

--- back/tr3-laravel/database/seeders/DatabaseSeeder.php
+++ back/tr3-laravel/database/seeders/DatabaseSeeder.php
@@ -21,7 +21,7 @@ class DatabaseSeeder extends Seeder
             $category -> save();
     
             $category = new Category;
-            $category -> name = "History";
+            $category -> name = "Places";
             $category -> save();
     
             $category = new Category;
@@ -46,12 +46,256 @@ class DatabaseSeeder extends Seeder
         }
 
         //General Knowledge
+        {
+            $word = new Word;
+            $word -> name = "Dog";
+            $word -> description = "A common household pet and loyal companion to humans";
+            $word -> category_id = 1;
+            $word -> save();
 
-        //History
+            $word = new Word;
+            $word -> name = "Rain";
+            $word -> description = "Water falling from the sky in droplets";
+            $word -> category_id = 1;
+            $word -> save();
+
+            $word = new Word;
+            $word -> name = "Tree";
+            $word -> description = "A perennial plant with a single stem or trunk";
+            $word -> category_id = 1;
+            $word -> save();
+
+            $word = new Word;
+            $word -> name = "Sun";
+            $word -> description = "The star at the center of the solar system";
+            $word -> category_id = 1;
+            $word -> save();
+            
+            $word = new Word;
+            $word -> name = "Mathematics";
+            $word -> description = "The study of numbers";
+            $word -> category_id = 1;
+            $word -> save();
+
+            $word = new Word;
+            $word -> name = "Car";
+            $word -> description = "A vehicle with four wheels, designed for transportation on roads";
+            $word -> category_id = 1;
+            $word -> save();
+
+            $word = new Word;
+            $word -> name = "Book";
+            $word -> description = "A written or printed work consisting of pages bound together";
+            $word -> category_id = 1;
+            $word -> save();
+
+            $word = new Word;
+            $word -> name = "Bird";
+            $word -> description = "A warm-blooded, egg-laying vertebrate with feathers and wings";
+            $word -> category_id = 1;
+            $word -> save();
+
+            $word = new Word;
+            $word -> name = "Clock";
+            $word -> description = "A device that tells time by means of a dial or digital display";
+            $word -> category_id = 1;
+            $word -> save();
+
+            $word = new Word;
+            $word -> name = "House";
+            $word -> description = "A building designed for people to live in";
+            $word -> category_id = 1;
+            $word -> save();
+        }
+
+        //Places
+        {
+            $word = new Word;
+            $word -> name = "Museum";
+            $word -> description = "A building or institution that exhibits objects of historical, cultural, or scientific significance";
+            $word -> category_id = 2;
+            $word -> save();
+
+            $word = new Word;
+            $word -> name = "Park";
+            $word -> description = "A public area designed for recreation and leisure   ";
+            $word -> category_id = 2;
+            $word -> save();
+
+            $word = new Word;
+            $word -> name = "Forest";
+            $word -> description = "A large area covered with trees and undergrowth";
+            $word -> category_id = 2;
+            $word -> save();
+
+            $word = new Word;
+            $word -> name = "Airport";
+            $word -> description = "A place where airplanes take off and land";
+            $word -> category_id = 2;
+            $word -> save();
+            
+            $word = new Word;
+            $word -> name = "Office";
+            $word -> description = "A place where people work";
+            $word -> category_id = 2;
+            $word -> save();
+
+            $word = new Word;
+            $word -> name = "City";
+            $word -> description = "A large urban area with a high population density";
+            $word -> category_id = 2;
+            $word -> save();
+
+            $word = new Word;
+            $word -> name = "Hospital";
+            $word -> description = "A place where people go for medical treatment";
+            $word -> category_id = 2;
+            $word -> save();
+
+            $word = new Word;
+            $word -> name = "Church";
+            $word -> description = "A place of worship for Christians";
+            $word -> category_id = 2;
+            $word -> save();
+
+            $word = new Word;
+            $word -> name = "Temple";
+            $word -> description = "A place of worship";
+            $word -> category_id = 2;
+            $word -> save();
+
+            $word = new Word;
+            $word -> name = "School";
+            $word -> description = "A place where children go to learn";
+            $word -> category_id = 2;
+            $word -> save();
+        }
 
         //Art
+        {
+            $word = new Word;
+            $word -> name = "Color";
+            $word -> description = "Property of an object, result of the way it reflects or emits light";
+            $word -> category_id = 3;
+            $word -> save();
+
+            $word = new Word;
+            $word -> name = "Brush";
+            $word -> description = "A tool used to apply paint to a surface";
+            $word -> category_id = 3;
+            $word -> save();
+
+            $word = new Word;
+            $word -> name = "Canvas";
+            $word -> description = "A fabric used for painting";
+            $word -> category_id = 3;
+            $word -> save();
+
+            $word = new Word;
+            $word -> name = "Palette";
+            $word -> description = "A flat board used by artists to mix and hold paint";
+            $word -> category_id = 3;
+            $word -> save();
+            
+            $word = new Word;
+            $word -> name = "Sketch";
+            $word -> description = "A quick drawing or outline intended to capture the basic elements of a subject";
+            $word -> category_id = 3;
+            $word -> save();
+
+            $word = new Word;
+            $word -> name = "Portrait";
+            $word -> description = "A representation of a person, typically the face";
+            $word -> category_id = 3;
+            $word -> save();
+
+            $word = new Word;
+            $word -> name = "Sculpture";
+            $word -> description = "A three-dimensional work of art created by shaping or combining materials";
+            $word -> category_id = 3;
+            $word -> save();
+
+            $word = new Word;
+            $word -> name = "Design";
+            $word -> description = "The process of creating a plan or layout for something";
+            $word -> category_id = 3;
+            $word -> save();
+
+            $word = new Word;
+            $word -> name = "Poem";
+            $word -> description = "A piece of writing in which the words rhyme";
+            $word -> category_id = 3;
+            $word -> save();
+
+            $word = new Word;
+            $word -> name = "Song";
+            $word -> description = "Short poem or other set of words set to music or meant to be sung.";
+            $word -> category_id = 3;
+            $word -> save();
+        }
 
         //Science
+        {
+            $word = new Word;
+            $word -> name = "Atom";
+            $word -> description = "The basic unit of a chemical element";
+            $word -> category_id = 4;
+            $word -> save();
+
+            $word = new Word;
+            $word -> name = "Gravity";
+            $word -> description = "The force that attracts two bodies toward each other";
+            $word -> category_id = 4;
+            $word -> save();
+
+            $word = new Word;
+            $word -> name = "Magnet";
+            $word -> description = "An object that produces a magnetic field";
+            $word -> category_id = 4;
+            $word -> save();
+
+            $word = new Word;
+            $word -> name = "Fossil";
+            $word -> description = "The remains of a prehistoric organism preserved in rock";
+            $word -> category_id = 4;
+            $word -> save();
+            
+            $word = new Word;
+            $word -> name = "Solar";
+            $word -> description = "Related to the sun or utilizing its energy";
+            $word -> category_id = 4;
+            $word -> save();
+
+            $word = new Word;
+            $word -> name = "Molecule";
+            $word -> description = "A group of atoms bonded together";
+            $word -> category_id = 4;
+            $word -> save();
+
+            $word = new Word;
+            $word -> name = "Ecosystem";
+            $word -> description = "A community of living and nonliving things interacting in a specific area";
+            $word -> category_id = 4;
+            $word -> save();
+
+            $word = new Word;
+            $word -> name = "Laser";
+            $word -> description = "A device that emits a narrow, intense beam of light";
+            $word -> category_id = 4;
+            $word -> save();
+
+            $word = new Word;
+            $word -> name = "Electricity";
+            $word -> description = "A form of energy resulting from the flow of electrons";
+            $word -> category_id = 4;
+            $word -> save();
+
+            $word = new Word;
+            $word -> name = "Sound";
+            $word -> description = "A type of energy that travels through the air in the form of waves";
+            $word -> category_id = 4;
+            $word -> save();
+        }
 
         //Sports
         {
@@ -105,7 +349,7 @@ class DatabaseSeeder extends Seeder
 
             $word = new Word;
             $word -> name = "Baseball";
-            $word -> description = "a 9vs9 bat game in which players try to hit a ball thrown by the opposing team's pitcher and then run around four bases";
+            $word -> description = "A 9vs9 bat game in which players try to hit a ball thrown by the opposing team's pitcher and then run around four bases";
             $word -> category_id = 5;
             $word -> save();
 
@@ -115,7 +359,69 @@ class DatabaseSeeder extends Seeder
             $word -> category_id = 5;
             $word -> save();
         }
+
         //Geography
+        {
+            $word = new Word;
+            $word -> name = "Continent";
+            $word -> description = "One of the seven large land masses on Earth";
+            $word -> category_id = 6;
+            $word -> save();
+
+            $word = new Word;
+            $word -> name = "River";
+            $word -> description = "A natural flowing watercourse, often emptying into an ocean";
+            $word -> category_id = 6;
+            $word -> save();
+
+            $word = new Word;
+            $word -> name = "Mountain";
+            $word -> description = "A large natural elevation of the earth's surface";
+            $word -> category_id = 6;
+            $word -> save();
+
+            $word = new Word;
+            $word -> name = "Island";
+            $word -> description = "A piece of land surrounded by water";
+            $word -> category_id = 6;
+            $word -> save();
+            
+            $word = new Word;
+            $word -> name = "Desert";
+            $word -> description = "A barren area of land where little precipitation occurs";
+            $word -> category_id = 6;
+            $word -> save();
+
+            $word = new Word;
+            $word -> name = "Ocean";
+            $word -> description = "A vast body of saltwater that covers almost 71 percent of the earth's surface";
+            $word -> category_id = 6;
+            $word -> save();
+
+            $word = new Word;
+            $word -> name = "Volcano";
+            $word -> description = "A mountain that opens downward to a pool of molten rock";
+            $word -> category_id = 6;
+            $word -> save();
+
+            $word = new Word;
+            $word -> name = "Peninsula";
+            $word -> description = "A piece of land almost surrounded by water";
+            $word -> category_id = 6;
+            $word -> save();
+
+            $word = new Word;
+            $word -> name = "Tundra";
+            $word -> description = "A vast, flat, treeless Arctic region of Europe, Asia, and North America";
+            $word -> category_id = 6;
+            $word -> save();
+
+            $word = new Word;
+            $word -> name = "Coast";
+            $word -> description = "The land near the sea or ocean";
+            $word -> category_id = 6;
+            $word -> save();
+        }
 
         //Computer Science
         {
