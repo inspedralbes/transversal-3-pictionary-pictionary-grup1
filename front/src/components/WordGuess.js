b8123aad8904f5482bc037aeb3e2eb419fc17987
GasparGomez <a21gasgomtor@inspedralbes.cat>
2023-03-15 13:08:13 +0100

    word guess fixed

--- front/src/components/WordGuess.js
+++ front/src/components/WordGuess.js
@@ -3,24 +3,27 @@ import { useState, useEffect } from "react";
 import React from 'react';
 
 function WordGuess({ socket }) {
-  const [wordToCheck, setWordToCheck] = useState("");
+    const [wordToCheck, setWordToCheck] = useState("");
 
-  useEffect(() => {
-    socket.on('game_data', (data) => {
-      setWordToCheck(data.words[0].name);
-    });
+    useEffect(() => {
+        socket.on('game_data', (data) => {
+            console.log(data);
+            if (data.words != undefined) {
+                setWordToCheck(data.words[0].name);
+            }
+        });
 
-    return () => {
-      socket.off('word_to_check');
-    };
-  }, []);
+        return () => {
+            socket.off('word_to_check');
+        };
+    }, []);
 
 
-  return (
-    <>
-      <p>{wordToCheck}</p>
-    </>
-  )
+    return (
+        <>
+            <p>{wordToCheck}</p>
+        </>
+    )
 
 }
 
