059f7da3c65f53b9094e2e7b62627a13a478781a
GasparGomez <a21gasgomtor@gmail.com>
2023-03-25 09:49:05 +0100

    description api quitada, ahora solo usamos las definiciones de bbdd

--- front/src/components/Ranking.js
+++ front/src/components/Ranking.js
@@ -13,19 +13,14 @@ function Ranking({ socket }) {
       socket.emit("lobby_data");
       setFirstTime(false)
     }
-    socket.on("lobby_user_list", (data) => {
 
-      console.log("Users list", data.list);
-      data.list.sort((a, b) =>  b.points-a.points);
+    socket.on("lobby_user_list", (data) => {
+      console.log(data)
+      data.list.sort((a, b) => b.points - a.points);
       setUserList(data.list);
     });
 
     socket.on('spectator', (data) => {
-      setSpectator(data.spectator);
-    });
-
-    socket.on('spectator', (data) => {
-      console.log("data", data);
       setSpectator(data.spectator);
     });
   }, [firstTime, socket])
@@ -34,7 +29,6 @@ function Ranking({ socket }) {
     <div>
       <div className="endgame__ranking">
         <h1>Ranking</h1>
-        <h2 className="connectedUsers_title">Connected users</h2>
         <ul id="userList" className="connectedUsers__userList userList">
           {userList.map((user, index) => {
             return (
