68ad91b9360f46c5e0568befa246976a235ea3ec
Marti-SV <a21marsalval@inspedralbes.cat>
2023-03-28 01:43:20 +0200

    array clear error

--- front/src/components/WordLength.js
+++ front/src/components/WordLength.js
@@ -3,45 +3,72 @@ import { useState, useEffect } from "react";
 import React from "react";
 
 function WordLength({ socket }) {
-    const [letters, setLetters] = useState([])
+  const [letters, setLetters] = useState([]);
+  const [word, setWord] = useState("");
 
-    const updateArray = (array) => {
-        setLetters(array);
+  const pushSpaces = () => {
+    setLetters((letters) => [
+      ...letters,
+      {
+        id: letters.length,
+        letter: "_ ",
+      },
+    ]);
+  };
+
+  const updateArray = (data) => {
+    const newLetters = letters.map((l) => {
+      console.log(l);
+      return {
+        ...l,
+        letter: data.letterNode,
+      };
+    });
+    setLetters(newLetters);
+  };
+
+  useEffect(() => {
+    socket.on("word_length", (data) => {
+      if (data !== undefined) {
+        if (data.long !== 0) {
+          for (let i = 0; i < data.long; i++) {
+            pushSpaces();
+          }
+        }
+      }
+    });
+
+    socket.on("word_letters", (data) => {
+      updateArray(data);
+    });
+
+    socket.on("clear_word", () => {
+      //updateArray([]);
+    });
+
+    socket.emit("word_length_loaded");
+  }, []);
+
+  useEffect(() => {
+    if (letters !== []) {
+      const lettersToString = letters.map((l) => {
+        return l["letter"];
+      });
+      setWord(lettersToString);
     }
+    console.log(letters);
+    console.log(word);
+  }, [letters]);
 
-    useEffect(() => {
-        socket.on("word_length", (data) => {
-            let spaces = [];
-            if (data !== undefined) {
-                if (data.long !== 0) {
-                    for (let i = 0; i < data.long; i++) {
-                        spaces.push("_ ");
-                    }
-                    updateArray(spaces);
-                }
-            }
-            
-        });
-        
-        socket.on("word_letters", (data) => {
-            console.log(letters);
-            let a = [...letters]
-            a[data.pos] = data.letterNode;
-            updateArray(a)
-        });
-
-        // socket.on("clear_word", () => {
-        //     updateArray([]);
-        // });
-
-        socket.emit("word_length_loaded");
-    }, []);
-
-    return (
-        <div>
-            <p>{letters}</p>
-        </div>
-    );
+  return (
+    <div>
+      <p>{word}</p>
+      <button onClick={() => updateArray({ pos: 3, letterNode: "M" })}>
+        {" "}
+        AWA
+      </button>
+    </div>
+  );
 }
 
 export default WordLength;
