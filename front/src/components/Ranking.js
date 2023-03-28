4ce652eab9e10a7e2b860bde47d4c45549ed17ea
Albert-de-la-Orza <A20albmar@inspedralbes.cat>
2023-03-28 13:57:45 +0200

    Añadidos estilos ranking

--- front/src/components/Ranking.js
+++ front/src/components/Ranking.js
@@ -2,8 +2,40 @@ import { useState, useEffect, useRef } from "react";
 import { render } from "react-dom";
 import "../styles/Ranking.css"
 
+const users = [
+  {
+    name: "Alumno 1",
+    points: 120,
+    avatar: " https://api.dicebear.com/6.x/bottts/svg?seed=Boo"
+
+  },
+  {
+    name: "Alumno 2",
+    points: 120,
+    avatar: " https://api.dicebear.com/6.x/bottts/svg?seed=Boo"
+
+  },
+  {
+    name: "Alumno 3",
+    points: 120,
+    avatar: " https://api.dicebear.com/6.x/bottts/svg?seed=Boo"
+
+  },
+  {
+    name: "Alumno 4",
+    points: 120,
+    avatar: " https://api.dicebear.com/6.x/bottts/svg?seed=Boo"
+
+  },
+  {
+    name: "Alumno 5",
+    points: 120,
+    avatar: " https://api.dicebear.com/6.x/bottts/svg?seed=Boo"
+
+  },
+]
 function Ranking({ socket }) {
-  const [userList, setUserList] = useState([]);
+  const [userList, setUserList] = useState(users);
   const [firstTime, setFirstTime] = useState(true);
   const [spectator, setSpectator] = useState(false);
 
@@ -21,7 +53,7 @@ function Ranking({ socket }) {
       item.classList.add('fadein')
     }, delay)
   }
-  
+
   useEffect(() => {
     if (firstTime) {
       socket.emit("lobby_data");
@@ -38,7 +70,7 @@ function Ranking({ socket }) {
     socket.on("spectator", (data) => {
       setSpectator(data.spectator);
     });
-    
+
   }, [firstTime, socket])
 
   return (
@@ -50,10 +82,18 @@ function Ranking({ socket }) {
           {userList.map((leader, index) => (
             <div key={leader.id}>
               {index + 1 <= 3 && (
-                  <div className="ranking__leader fade-item">
-                    <img src={leader.avatar}></img>
+                <div className="ranking__leader fade-item">
+                  <div className="leader__data">
+                    <div className="crown">
+                      <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-crown" width="100" height="100" viewBox="0 0 24 24" stroke-width="3" stroke="#ffec00" fill="gold" stroke-linecap="round" stroke-linejoin="round">
+                        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                        <path d="M12 6l4 6l5 -4l-2 10h-14l-2 -10l5 4z" />
+                      </svg>
+                    </div>
+                    <img className="leader__avatar" src={leader.avatar}></img>
                     <div className="leader__name">{leader.name}</div>
-                    <div className="leader__points">{leader.points} points</div>
+                    <div className="leader__points"><p>{leader.points} points</p></div>
+                  </div>
                 </div>
               )}
             </div>
