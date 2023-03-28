8cb825da381676fe4507f6065031e2b1fd1b1fec
Albert <A20albmar@inspedralbes.cat>
2023-03-29 00:25:47 +0200

    Añadidos los avatares al ranking

--- front/src/components/Ranking.js
+++ front/src/components/Ranking.js
@@ -2,40 +2,8 @@ import { useState, useEffect, useRef } from "react";
 import { render } from "react-dom";
 import "../styles/Ranking.css"
 
-const users = [
-  {
-    name: "Alumno 1",
-    points: 120,
-    avatar: " https://api.dicebear.com/6.x/bottts/svg?seed=Boo"
-
-  },
-  {
-    name: "Alumno 2",
-    points: 120,
-    avatar: " https://api.dicebear.com/6.x/bottts/svg?seed=Boo"
-
-  },
-  {
-    name: "Alumno 3",
-    points: 120,
-    avatar: " https://api.dicebear.com/6.x/bottts/svg?seed=Boo"
-
-  },
-  {
-    name: "Alumno 4",
-    points: 120,
-    avatar: " https://api.dicebear.com/6.x/bottts/svg?seed=Boo"
-
-  },
-  {
-    name: "Alumno 5",
-    points: 120,
-    avatar: " https://api.dicebear.com/6.x/bottts/svg?seed=Boo"
-
-  },
-]
 function Ranking({ socket }) {
-  const [userList, setUserList] = useState(users);
+  const [userList, setUserList] = useState([]);
   const [firstTime, setFirstTime] = useState(true);
   const [spectator, setSpectator] = useState(false);
 
@@ -77,23 +45,43 @@ function Ranking({ socket }) {
     <div>
       <h1>Ranking</h1>
 
-      <div id="border" className="ranking__container">
+      <div className="ranking__container">
         <div className="topLeadersList fire">
           {userList.map((leader, index) => (
             <div key={leader.id}>
               {index + 1 <= 3 && (
                 <div className="ranking__leader fade-item">
-                  <div className="leader__data">
-                    <div className="crown">
-                      <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-crown" width="100" height="100" viewBox="0 0 24 24" stroke-width="3" stroke="#ffec00" fill="gold" stroke-linecap="round" stroke-linejoin="round">
-                        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                        <path d="M12 6l4 6l5 -4l-2 10h-14l-2 -10l5 4z" />
-                      </svg>
-                    </div>
-                    <img className="leader__avatar" src={leader.avatar}></img>
-                    <div className="leader__name">{leader.name}</div>
-                    <div className="leader__points"><p>{leader.points} points</p></div>
+                  <div className="crown">
+                    {(() => {
+                      if (index == 1) {
+                        return (
+                          <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-crown" width="100" height="100" viewBox="0 0 24 24" stroke-width="3" stroke="#597e8d" fill="rgb(192, 192, 192)" stroke-linecap="round" stroke-linejoin="round">
+                            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                            <path d="M12 6l4 6l5 -4l-2 10h-14l-2 -10l5 4z" />
+                          </svg>
+                        )
+                      } else if (index == 2) {
+                        return (
+                          <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-crown" width="100" height="100" viewBox="0 0 24 24" stroke-width="3" stroke="#ff9300" fill="#ffbf00" stroke-linecap="round" stroke-linejoin="round">
+                            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                            <path d="M12 6l4 6l5 -4l-2 10h-14l-2 -10l5 4z" />
+                          </svg>
+                        )
+                      } else {
+                        return (
+                          <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-crown" width="100" height="100" viewBox="0 0 24 24" stroke-width="3" stroke="#ffec00" fill="gold" stroke-linecap="round" stroke-linejoin="round">
+                            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                            <path d="M12 6l4 6l5 -4l-2 10h-14l-2 -10l5 4z" />
+                          </svg>
+                        )
+                      }
+                    })()}
+
+
                   </div>
+                  <img className="leader__avatar" src={leader.avatar}></img>
+                  <div className="leader__name">{leader.name}</div>
+                  <div className="leader__points"><p>{leader.points} points</p></div>
                 </div>
               )}
             </div>
@@ -109,7 +97,7 @@ function Ranking({ socket }) {
 
                     <div>
                       <span>{index}</span>
-                      <img src={leader.avatar}></img>
+                      <img className="avatar__img" src={leader.avatar}></img>
                       <span className="player__name"> {leader.name} </span>
                       <span className="player__points"> {leader.points} points</span>
                     </div>
