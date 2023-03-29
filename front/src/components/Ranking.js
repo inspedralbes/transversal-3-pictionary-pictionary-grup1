a67429c33b27941e9e0bb18eda7ad0b8009c6a58
Albert-de-la-Orza <A20albmar@inspedralbes.cat>
2023-03-29 09:35:40 +0200

    Corregidos errores del ranking

--- front/src/components/Ranking.js
+++ front/src/components/Ranking.js
@@ -2,6 +2,7 @@ import { useState, useEffect, useRef } from "react";
 import { render } from "react-dom";
 import "../styles/Ranking.css"
 
+
 function Ranking({ socket }) {
   const [userList, setUserList] = useState([]);
   const [firstTime, setFirstTime] = useState(true);
@@ -48,26 +49,26 @@ function Ranking({ socket }) {
           {userList.map((leader, index) => (
             <div key={leader.id}>
               {index + 1 <= 3 && (
-                <div className="ranking__leader fade-item">
+                <div key={index} className="ranking__leader fade-item">
                   <div className="crown">
                     {(() => {
                       if (index == 1) {
                         return (
-                          <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-crown" width="100" height="100" viewBox="0 0 24 24" stroke-width="3" stroke="#597e8d" fill="rgb(192, 192, 192)" stroke-linecap="round" stroke-linejoin="round">
+                          <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-crown" width="100" height="100" viewBox="0 0 24 24" strokeWidth="3" stroke="#597e8d" fill="rgb(192, 192, 192)" strokeLinecap="round" strokeLinejoin="round">
                             <path stroke="none" d="M0 0h24v24H0z" fill="none" />
                             <path d="M12 6l4 6l5 -4l-2 10h-14l-2 -10l5 4z" />
                           </svg>
                         )
                       } else if (index == 2) {
                         return (
-                          <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-crown" width="100" height="100" viewBox="0 0 24 24" stroke-width="3" stroke="#ff9300" fill="#ffbf00" stroke-linecap="round" stroke-linejoin="round">
+                          <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-crown" width="100" height="100" viewBox="0 0 24 24" strokeWidth="3" stroke="#ff9300" fill="#ffbf00" strokeLinecap="round" strokeLinejoin="round">
                             <path stroke="none" d="M0 0h24v24H0z" fill="none" />
                             <path d="M12 6l4 6l5 -4l-2 10h-14l-2 -10l5 4z" />
                           </svg>
                         )
                       } else {
                         return (
-                          <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-crown" width="100" height="100" viewBox="0 0 24 24" stroke-width="3" stroke="#ffec00" fill="gold" stroke-linecap="round" stroke-linejoin="round">
+                          <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-crown" width="100" height="100" viewBox="0 0 24 24" strokeWidth="3" stroke="#ffec00" fill="gold" strokeLinecap="round" strokeLinejoin="round">
                             <path stroke="none" d="M0 0h24v24H0z" fill="none" />
                             <path d="M12 6l4 6l5 -4l-2 10h-14l-2 -10l5 4z" />
                           </svg>
@@ -91,14 +92,11 @@ function Ranking({ socket }) {
             {userList.map((leader, index) => (
               <div className="player" key={leader.id}>
                 <span> {index + 1 > 3 && (
-                  <div className="ranking__player">
-
-                    <div>
-                      <span>{index}</span>
+                  <div key={index} className="ranking__player">
+                      <p className="player__index">{index}</p>
                       <img className="avatar__img" src={leader.avatar}></img>
-                      <span className="player__name"> {leader.name} </span>
-                      <span className="player__points"> {leader.points} points</span>
-                    </div>
+                      <p className="player__name"> {leader.name} </p>
+                      <p className="player__points"> {leader.points} points</p>
                   </div>
                 )}</span>
               </div>
