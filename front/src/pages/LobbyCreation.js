f805cd4955515ee5ba7a4bf6caadf95f5ba54bbe
a21alecrinor <a21alecrinor@inspedralbes.cat>
2023-03-29 10:55:51 +0200

    Create fet

--- front/src/pages/LobbyCreation.js
+++ front/src/pages/LobbyCreation.js
@@ -155,7 +155,7 @@ function LobbyCreation({ socket }) {
                 <section id="main">
                   <div>
                     <Tabs socket={socket} start={starting} categoriesData={categoriesData}></Tabs>
-                  </div>
+                  </div>  
                 </section>
                 <div className="createGame__startButtonDiv">
                   <button
